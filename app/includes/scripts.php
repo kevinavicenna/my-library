b37be288650b1cfccf65b096303e24d46c2220e1
Muhammad Imaduddin Mustofa <l200200165@student.ums.ac.id>
2023-07-13 23:22:55 +0700

    misahin struktur file dari index.php jadi berapa bagian

---
+++ app/includes/scripts.php
@@ -0,0 +1,12 @@
+<script src="./assets/static/js/components/dark.js"></script>
+<script src="./assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+<script src="./assets/compiled/js/app.js"></script>
+
+<!-- Need: Apexcharts -->
+<script src="./assets/extensions/apexcharts/apexcharts.min.js"></script>
+<script src="./assets/static/js/pages/dashboard.js"></script>
+
+<!-- Datatable -->
+<script src="assets/extensions/simple-datatables/umd/simple-datatables.js"></script>
+<script src="assets/static/js/pages/simple-datatables.js"></script>
