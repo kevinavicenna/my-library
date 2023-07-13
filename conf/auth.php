04a9f08208de116d282c3cfbc5c35812d820873f
Muhammad Imaduddin Mustofa <l200200165@student.ums.ac.id>
2023-07-13 23:29:42 +0700

    tambahan parameter utk kondisi login tanpa username n password

--- conf/auth.php
+++ conf/auth.php
@@ -7,6 +7,8 @@ $password = $_POST['password'];
 $query = mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username' AND password='$password'");
 if(mysqli_num_rows($query)==1){
     header('Location:../app');
+}else if($username == '' || $password == ''){
+    header('Location:../index.php?error=2');
 }else{
     header('Location:../index.php?error=1');
 }
