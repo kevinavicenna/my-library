e0149fd3ad1a67ca7c9961b0b15b3f202a7b205c
Muhammad Imaduddin Mustofa <l200200165@student.ums.ac.id>
2023-07-13 14:32:05 +0700

    mindahin file config.php ke folder conf agar lebih terstruktur

---
+++ conf/config.php
@@ -0,0 +1,11 @@
+<?php
+$koneksi = mysqli_connect('localhost', 'root', '', 'library');
+
+//Cek Koneksi
+
+// if(!$koneksi){
+//     die("Koneksi Gagal : ". mysqli_connect_error());
+// }else{
+//     echo "Koneksi Berhasil";
+// }
+?>
