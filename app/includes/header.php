c83c504faed93d56c385f7c905ab610c2331d17e
Muhammad Imaduddin Mustofa <l200200165@student.ums.ac.id>
2023-07-19 14:47:41 +0700

    nambahin logo

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,11 +1,11 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dashboard - UMS Library Dashboard</title>
+    <title>UMS Library</title>
 
     <link
       rel="shortcut icon"
-      href="./assets/compiled/svg/favicon.svg"
+      href="./assets/compiled/svg/ums-logo.svg"
       type="image/x-icon"
     />
     <link
--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -3,11 +3,12 @@
           <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
               <div class="logo">
-                <a href="index.php"
+                <a href="index.php?=dashboard"
                   ><img
-                    src="./assets/compiled/svg/logo.svg"
+                    src="./assets/compiled/svg/ums-library.svg"
                     alt="Logo"
                     srcset=""
+                    height="50px"
                 /></a>
               </div>
               <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
